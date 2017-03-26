0e0a92e0f991e39a411cd0de69f8e215d81ed4a3
hephlathio <larsfkjelsaas@gmail.com>
2017-03-26 14:54:36 +0200

    Create model classes from previous project

---
+++ app/src/main/java/com/ntnu/swipeitagain/Models/CardModel.java
@@ -0,0 +1,61 @@
+package com.ntnu.swipeitagain.Models;
+
+import sheep.game.Sprite;
+
+/**
+ * Created by Lars on 26.03.2017.
+ */
+
+public class CardModel extends Sprite{
+    private Direction direction;
+//    private Texture arrowTexture;
+
+//    public Texture getTexture() {
+//        return arrowTexture;
+//    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public CardModel(Direction direction){
+
+        //Try-catch to be able to test without instanitating game
+        try {
+//            this.arrowTexture = new Texture("pil.gif");
+//          setTexture(arrowTexture);
+        }catch(NullPointerException e){
+            System.out.println("Error creating texture");
+        }
+        this.direction = direction;
+//        scaleDirection(direction);
+
+    }
+
+    // Scales the texture according to the direction
+
+ /*
+    private void scaleDirection(Direction direction){
+        if(direction == Direction.LEFT){
+            this.setScale(-1, 1);
+        }else if (direction == Direction.DOWN){
+            this.rotate90(true);
+        }else if (direction == Direction.UP){
+            this.rotate90(false);
+        }
+
+    }*/
+    private void setTexture(){
+
+    }
+
+
+
+    //
+
+    /*
+    private int value;
+    private String suit;
+    */
+}
+
---
+++ app/src/main/java/com/ntnu/swipeitagain/Models/GameModel.java
@@ -0,0 +1,59 @@
+package com.ntnu.swipeitagain.Models;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by Lars on 26.03.2017.
+ */
+
+public class GameModel {
+
+    private ArrayList<CardModel> cards;
+    private CardModel currentCard;
+    private PlayerModel player;
+
+    //Constructor
+    public GameModel(){
+        createCards();
+        player = new PlayerModel();
+    }
+
+    //gets true if still time left
+    public boolean timeLeft(){
+        return player.timeLeft();
+    }
+
+
+    //Makes card for each direction
+    private void createCards(){
+        cards = new ArrayList<CardModel>();
+        for(Direction dir : Direction.values()){
+            cards.add(new CardModel(dir));
+        }
+    }
+
+    //compares direction from user with card-direction. if same, return true and go to next card
+    public boolean checkDirection(Direction playerDirection){
+        Direction cardDirection = currentCard.getDirection();
+        if(cardDirection == playerDirection){
+            nextCard();
+            return true;
+        }
+        else return false;
+    }
+
+    //sets current card to a new, random card
+    public void nextCard(){
+        Random r = new Random();
+        currentCard = cards.get(r.nextInt(Direction.values().length));
+    }
+
+    public CardModel getCurrentCard(){
+        return currentCard;
+    }
+
+    public ArrayList<CardModel> getCards(){
+        return cards;
+    }
+}

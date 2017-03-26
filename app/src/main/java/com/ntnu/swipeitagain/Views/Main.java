a7a67a4cefec18885277c0960cb08183e74919c2
mariessa <marie.sagedal@hotmail.com>
2017-03-26 16:12:34 +0200

    add serverCommunictor to Main

--- app/src/main/java/com/ntnu/swipeitagain/Views/Main.java
+++ app/src/main/java/com/ntnu/swipeitagain/Views/Main.java
@@ -11,12 +11,14 @@ import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
 import com.ntnu.swipeitagain.Controllers.BoardController;
+import com.ntnu.swipeitagain.Controllers.ServerCommunicator;
 
 import sheep.game.Game;
 public class Main extends Activity{
     private BoardController boardController;
     public int SCREEN_WIDTH;
     public int SCREEN_HEIGHT;
+    ServerCommunicator sc = new ServerCommunicator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -32,5 +34,6 @@ public class Main extends Activity{
         this.boardController = new BoardController(game);
         game.pushState(new MainMenu(game, game.getResources(), SCREEN_WIDTH, SCREEN_HEIGHT));
         setContentView(game);
+        sc.connectSocket();
     }
 }

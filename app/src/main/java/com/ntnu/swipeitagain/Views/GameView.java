715cb43a15db9872336ee4b08a3d0f172f3174e3
henloef <henloef@gmail.com>
2017-04-07 08:44:46 +0200

    move time bar

--- app/src/main/java/com/ntnu/swipeitagain/Views/GameView.java
+++ app/src/main/java/com/ntnu/swipeitagain/Views/GameView.java
@@ -58,6 +58,7 @@ public abstract class GameView extends State implements WidgetListener {
     public boolean onTouchDown(MotionEvent motionEvent) {
         if (gameModel.getCurrentCard().getBoundingBox().contains(motionEvent.getX(),  motionEvent.getY())) {
             startEvent = motionEvent;
+            swiped = true;
             return true;
         }
         //boardController.pushState(new GameOver(boardController, screenWidth, screenHeight));
@@ -84,6 +85,9 @@ public abstract class GameView extends State implements WidgetListener {
             gameModel.getCurrentCard().setScale(1,1);
             gameModel.getCurrentCard().update(0.1f);
             Log.d(TAG, "Swiped X: " + motionEvent.getX()+ " Y: " + motionEvent.getY());
+
+
+
             return true;
 
         }
@@ -93,20 +97,27 @@ public abstract class GameView extends State implements WidgetListener {
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        canvas.drawColor(Color.BLUE);
+        canvas.drawColor(Color.LTGRAY);
         goDirectlyToGameOver.draw(canvas);
 
         int time = gameModel.getCurrentTime();
         Log.d(TAG, "current time: " + time);
         double progress = (1.0 * time)/100;
         int barEnd = 100+(int)(screenWidth * (progress * ((1.0 * screenWidth - 200) / screenWidth)));
-        Rect rect = new Rect(100, screenHeight - 300, barEnd , screenHeight - 250);
+        Rect rect = new Rect(100, screenHeight - 200, barEnd , screenHeight - 170);
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
         paint.setColor(Color.argb(255,255 - (int)(progress*255),(int) (progress *255),0)); //gradually from green to red
         canvas.drawRect(rect, paint);
 
+        //positions card in the middle
+        gameModel.getCurrentCard().setScale(1,1);
+        gameModel.getCurrentCard().update(0.2f);
+        gameModel.getCurrentCard().setPosition((float)screenWidth/2, (float)screenHeight/2);
         gameModel.getCurrentCard().draw(canvas);
+
+
+
     }
 
     @Override
